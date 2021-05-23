fac6fb071d05f95cb02ace3ea125379dd9470b2e
AVENGER-OP <83777476+AVENGER-OP@users.noreply.github.com>
2021-05-23 22:50:28 +0530

    update queue.ts

--- src/handlers/queue.ts
+++ src/handlers/queue.ts
@@ -1,11 +1,4 @@
-/**
-* Ultroid - UserBot
-* Copyright (C) 2020 TeamUltroid
-*
-* This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-* PLease read the GNU Affero General Public License in
-* <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-**/
+
 
 import { Composer } from 'telegraf';
 import { getQueue, leaveVc, getCurrentSong, closeConnection } from '../tgcalls';
