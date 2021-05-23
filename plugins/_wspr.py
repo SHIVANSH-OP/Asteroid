19ff9f80c618d0b1dd62e280582fa24957684fa0
TEAMROYAL <77516086+TEAMROYAL@users.noreply.github.com>
2021-05-23 20:47:23 +0530

    update _wspr.py

--- plugins/_wspr.py
+++ plugins/_wspr.py
@@ -1,9 +1,4 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 
 import re
 
@@ -25,14 +20,14 @@ snap = {}
 buddhhu = []
 
 
-@ultroid_cmd(
+@Asteriod_cmd(
     pattern="wspr ?(.*)",
 )
 async def _(e):
     if e.reply_to_msg_id:
         okk = (await e.get_reply_message()).sender_id
         try:
-            zyx = await ultroid_bot(gu(id=okk))
+            zyx = await Asteriod_bot(gu(id=okk))
             put = zyx.user.username
         except ValueError as ex:
             return await eor(e, str(ex))
@@ -42,7 +37,7 @@ async def _(e):
         put = e.pattern_match.group(1)
     if put:
         try:
-            results = await ultroid_bot.inline_query(asst.me.username, f"msg {put}")
+            results = await Asteriod_bot.inline_query(asst.me.username, f"msg {put}")
         except rep:
             return await eor(
                 e,
@@ -73,7 +68,7 @@ async def _(e):
         desc = "Touch me"
     if "wspr" not in vvv:
         try:
-            logi = await ultroid_bot(gu(id=query))
+            logi = await Asteriod_bot(gu(id=query))
             name = logi.user.first_name
             ids = logi.user.id
             username = logi.user.username
@@ -118,7 +113,7 @@ async def _(e):
             )
     else:
         try:
-            logi = await ultroid_bot.get_entity(query)
+            logi = await Asteriod_bot.get_entity(query)
             button = [
                 Button.inline("Secret Msg", data=f"dd_{logi.id}"),
                 Button.inline("Delete Msg", data=f"del"),
