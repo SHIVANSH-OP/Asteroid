cc74b2f37c266b645d3fe2df961d634bb58041e3
TEAMROYAL <77516086+TEAMROYAL@users.noreply.github.com>
2021-05-23 20:38:24 +0530

    update _asteroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -1,9 +1,4 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 
 
 from telethon.errors import ChatSendInlineForbiddenError
@@ -12,19 +7,19 @@ from telethon.errors.rpcerrorlist import BotMethodInvalidError as bmi
 from . import *
 
 REPOMSG = (
-    "• **ULTROID USERBOT** •\n\n",
-    "• Repo - [Click Here](https://github.com/TeamUltroid/Ultroid)\n",
-    "• Addons - [Click Here](https://github.com/TeamUltroid/UltroidAddons)\n",
-    "• Support - @UltroidSupport",
+    "• **ASTEROID USERBOT** •\n\n",
+    "• Repo - [Click Here](https://github.com/TEAMROYAL/Asteroid)\n",
+    "• Addons - [Click Here](https://github.com/TEAMROYAL/AsteroidAddons)\n",
+    "• Support - @Asteriod_support",
 )
 
 
-@ultroid_cmd(pattern="repo$")
+@Asteroid_cmd(pattern="repo$")
 async def repify(e):
     try:
-        q = await ultroid_bot.inline_query(asst.me.username, "repo")
+        q = await Asteriod_bot.inline_query(asst.me.username, "repo")
         await q[0].click(e.chat_id)
-        if e.sender_id == ultroid_bot.uid:
+        if e.sender_id == Asteriod_bot.uid:
             await e.delete()
     except (ChatSendInlineForbiddenError, bmi):
         await eor(e, REPOMSG)
