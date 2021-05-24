5db842bc67aaae19b2394ce333c3af8210a2c349
AVENGER-OP <83777476+AVENGER-OP@users.noreply.github.com>
2021-05-24 07:35:14 +0530

    update dayevents.py

--- plugins/dayevents.py
+++ plugins/dayevents.py
@@ -1,9 +1,4 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 
 """
 ✘ Commands Available -
@@ -21,7 +16,7 @@ from bs4 import BeautifulSoup as bs
 from . import *
 
 
-@ultroid_cmd(pattern="eod ?(.*)")
+@Asteroid_cmd(pattern="eod ?(.*)")
 async def diela(e):
     match = e.pattern_match.group(1)
     m = await eor(e, "`Processing... `")
