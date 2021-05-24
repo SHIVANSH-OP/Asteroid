d459b780f09d5ec7286f5b76cf2e89123c959b60
TEAMROYAL <77516086+TEAMROYAL@users.noreply.github.com>
2021-05-24 10:44:21 +0530

    update profile.py

--- plugins/profile.py
+++ plugins/profile.py
@@ -1,9 +1,4 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 
 """
 ✘ Commands Available -
@@ -42,14 +37,14 @@ TMP_DOWNLOAD_DIRECTORY = "resources/downloads/"
 # bio changer
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="setbio ?(.*)",
 )
 async def _(ult):
     ok = await eor(ult, "...")
     set = ult.pattern_match.group(1)
     try:
-        await ultroid_bot(UpdateProfileRequest(about=set))
+        await Asteroid_bot(UpdateProfileRequest(about=set))
         await ok.edit(f"Profile bio changed to\n`{set}`")
     except Exception as ex:
         await ok.edit("Error occured.\n`{}`".format(str(ex)))
@@ -60,7 +55,7 @@ async def _(ult):
 # name changer
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="setname ?((.|//)*)",
 )
 async def _(ult):
@@ -71,7 +66,7 @@ async def _(ult):
     if "//" in names:
         first_name, last_name = names.split("//", 1)
     try:
-        await ultroid_bot(
+        await Asteroid_bot(
             UpdateProfileRequest(
                 first_name=first_name,
                 last_name=last_name,
@@ -87,7 +82,7 @@ async def _(ult):
 # profile pic
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="setpic$",
 )
 async def _(ult):
@@ -98,15 +93,15 @@ async def _(ult):
         os.makedirs(TMP_DOWNLOAD_DIRECTORY)
     photo = None
     try:
-        photo = await ultroid_bot.download_media(reply_message, TMP_DOWNLOAD_DIRECTORY)
+        photo = await Asteroid_bot.download_media(reply_message, TMP_DOWNLOAD_DIRECTORY)
     except Exception as ex:
         await ok.edit("Error occured.\n`{}`".format(str(ex)))
     else:
         if photo:
             await ok.edit("`Uploading it to my profile...`")
-            file = await ultroid_bot.upload_file(photo)
+            file = await Asteroid_bot.upload_file(photo)
             try:
-                await ultroid_bot(UploadProfilePhotoRequest(file))
+                await Asteroid_bot(UploadProfilePhotoRequest(file))
             except Exception as ex:
                 await ok.edit("Error occured.\n`{}`".format(str(ex)))
             else:
@@ -122,7 +117,7 @@ async def _(ult):
 # delete profile pic(s)
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="delpfp ?(.*)",
 )
 async def remove_profilepic(delpfp):
@@ -134,7 +129,7 @@ async def remove_profilepic(delpfp):
         lim = int(group)
     else:
         lim = 1
-    pfplist = await ultroid_bot(
+    pfplist = await Asteroid_bot(
         GetUserPhotosRequest(user_id=delpfp.from_id, offset=0, max_id=0, limit=lim),
     )
     input_photos = []
@@ -146,13 +141,13 @@ async def remove_profilepic(delpfp):
                 file_reference=sep.file_reference,
             ),
         )
-    await ultroid_bot(DeletePhotosRequest(id=input_photos))
+    await Asteroid_bot(DeletePhotosRequest(id=input_photos))
     await ok.edit(f"`Successfully deleted {len(input_photos)} profile picture(s).`")
     await asyncio.sleep(10)
     await ok.delete()
 
 
-@ultroid_cmd(pattern="poto ?(.*)")
+@Asteroid_cmd(pattern="poto ?(.*)")
 async def gpoto(e):
     ult = e.pattern_match.group(1)
     a = await eor(e, "`Processing...`")
@@ -162,13 +157,13 @@ async def gpoto(e):
     if not (ult or e.is_reply):
         ult = e.chat_id
     try:
-        okla = await ultroid_bot.download_profile_photo(
+        okla = await Asteroid_bot.download_profile_photo(
             ult,
             "profile.jpg",
             download_big=True,
         )
         await a.delete()
-        await ultroid_bot.send_message(e.chat_id, file=okla)
+        await Asteroid_bot.send_message(e.chat_id, file=okla)
         os.remove(okla)
     except Exception as er:
         await eor(e, f"ERROR - {str(er)}")
