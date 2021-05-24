fe10c9723a556b71d8fbd345fdfb93d749e362f2
TEAMROYAL <77516086+TEAMROYAL@users.noreply.github.com>
2021-05-24 11:14:28 +0530

    update utilities.py

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -1,9 +1,4 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 
 """
 ✘ Commands Available -
@@ -83,13 +78,13 @@ telegraph.create_account(short_name=OWNER_NAME)
 # ================================================================#
 
 
-@ultroid_cmd(pattern="kickme$", groups_only=True, allow_sudo=False)
+@Asteroid_cmd(pattern="kickme$", groups_only=True, allow_sudo=False)
 async def leave(ult):
-    await eor(ult, f"`{ultroid_bot.me.first_name} has left this group, bye!!.`")
-    await ultroid_bot(LeaveChannelRequest(ult.chat_id))
+    await eor(ult, f"`{Asteroid_bot.me.first_name} has left this group, bye!!.`")
+    await Asteroid_bot(LeaveChannelRequest(ult.chat_id))
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="date$",
 )
 async def date(event):
@@ -98,10 +93,10 @@ async def date(event):
     y = dt.now(k).year
     d = dt.now(k).strftime("Date - %B %d, %Y\nTime- %H:%M:%S")
     k = calendar.month(y, m)
-    ultroid = await eor(event, f"`{k}\n\n{d}`")
+    Asteroid = await eor(event, f"`{k}\n\n{d}`")
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="chatinfo(?: |$)(.*)",
 )
 async def info(event):
@@ -118,13 +113,13 @@ async def info(event):
     return
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="listreserved$",
 )
 async def _(event):
     if BOT_MODE:
         return await eor(ult, "You Cant Use this Command in BOT MODE")
-    result = await ultroid_bot(GetAdminedPublicChannelsRequest())
+    result = await Asteroid_bot(GetAdminedPublicChannelsRequest())
     output_str = ""
     r = result.chats
     for channel_obj in r:
@@ -135,7 +130,7 @@ async def _(event):
         await eor(event, output_str)
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="stats$",
 )
 async def stats(
@@ -156,7 +151,7 @@ async def stats(
     unread_mentions = 0
     unread = 0
     dialog: Dialog
-    async for dialog in ultroid_bot.iter_dialogs():
+    async for dialog in Asteroid_bot.iter_dialogs():
         entity = dialog.entity
         if isinstance(entity, Channel):
             if entity.broadcast:
@@ -188,8 +183,8 @@ async def stats(
         unread_mentions += dialog.unread_mentions_count
         unread += dialog.unread_count
     stop_time = time.time() - start_time
-    ct = (await ultroid_bot(GetBlockedRequest(1, 0))).count
-    full_name = inline_mention(await ultroid_bot.get_me())
+    ct = (await Asteroid_bot(GetBlockedRequest(1, 0))).count
+    full_name = inline_mention(await Asteroid_bot.get_me())
     response = f"🔸 **Stats for {full_name}** \n\n"
     response += f"**Private Chats:** {private_chats} \n"
     response += f"**  •• **`Users: {private_chats - bots}` \n"
@@ -209,7 +204,7 @@ async def stats(
     await ok.edit(response)
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="paste( (.*)|$)",
 )
 async def _(event):
@@ -262,14 +257,14 @@ async def _(event):
     q = f"paste {key}"
     reply_text = f"• **Pasted to Nekobin :** [Neko](https://nekobin.com/{key})\n• **Raw Url :** : [Raw](https://nekobin.com/raw/{key})"
     try:
-        ok = await ultroid_bot.inline_query(asst.me.username, q)
+        ok = await Asteroid_bot.inline_query(asst.me.username, q)
         await ok[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
         await xx.delete()
     except BaseException:
         await xx.edit(reply_text)
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="info ?(.*)",
 )
 async def _(event):
@@ -356,7 +351,7 @@ async def _(event):
     await xx.delete()
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="invite ?(.*)",
     groups_only=True,
 )
@@ -368,7 +363,7 @@ async def _(ult):
     if not ult.is_channel and ult.is_group:
         for user_id in to_add_users.split(" "):
             try:
-                await ultroid_bot(
+                await Asteroid_bot(
                     AddChatUserRequest(
                         chat_id=ult.chat_id,
                         user_id=user_id,
@@ -381,7 +376,7 @@ async def _(ult):
     else:
         for user_id in to_add_users.split(" "):
             try:
-                await ultroid_bot(
+                await Asteroid_bot(
                     InviteToChannelRequest(
                         channel=ult.chat_id,
                         users=[user_id],
@@ -392,7 +387,7 @@ async def _(ult):
                 await xx.edit(str(e))
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern=r"rmbg$",
 )
 async def rmbg(event):
@@ -404,7 +399,7 @@ async def rmbg(event):
         )
     if event.reply_to_msg_id:
         reply = await event.get_reply_message()
-        dl = await ultroid_bot.download_media(reply.media)
+        dl = await Asteroid_bot.download_media(reply.media)
         if not dl.endswith(("webp", "jpg", "png", "jpeg")):
             os.remove(dl)
             return await xx.edit("`Unsupported Media`")
@@ -430,19 +425,19 @@ async def rmbg(event):
     if zz.mode != "RGB":
         zz.convert("RGB")
     zz.save("ult.webp", "webp")
-    await ultroid_bot.send_file(
+    await Asteroid_bot.send_file(
         event.chat_id,
         rmbgp,
         force_document=True,
         reply_to=reply,
     )
-    await ultroid_bot.send_file(event.chat_id, "ult.webp", reply_to=reply)
+    await Asteroid_bot.send_file(event.chat_id, "ult.webp", reply_to=reply)
     os.remove(rmbgp)
     os.remove("ult.webp")
     await xx.delete()
 
 
-@ultroid_cmd(
+@Asteroid_cmd(
     pattern="telegraph ?(.*)",
 )
 async def telegraphcmd(event):
@@ -451,7 +446,7 @@ async def telegraphcmd(event):
     if event.reply_to_msg_id:
         getmsg = await event.get_reply_message()
         if getmsg.photo or getmsg.video or getmsg.gif:
-            getit = await ultroid_bot.download_media(getmsg)
+            getit = await Asteroid_bot.download_media(getmsg)
             try:
                 variable = uf(getit)
                 os.remove(getit)
@@ -461,14 +456,14 @@ async def telegraphcmd(event):
                 amsg = f"Error - {e}"
             await xx.edit(amsg)
         elif getmsg.document:
-            getit = await ultroid_bot.download_media(getmsg)
+            getit = await Asteroid_bot.download_media(getmsg)
             ab = open(getit)
             cd = ab.read()
             ab.close()
             if input_str:
                 tcom = input_str
             else:
-                tcom = "Ultroid"
+                tcom = "Asteroid"
             makeit = telegraph.create_page(title=tcom, content=[f"{cd}"])
             war = makeit["url"]
             os.remove(getit)
@@ -477,7 +472,7 @@ async def telegraphcmd(event):
             if input_str:
                 tcom = input_str
             else:
-                tcom = "Ultroid"
+                tcom = "Asteroid"
             makeit = telegraph.create_page(title=tcom, content=[f"{getmsg.text}"])
             war = makeit["url"]
             await xx.edit(f"Pasted to Telegraph : [Telegraph]({war})")
@@ -487,7 +482,7 @@ async def telegraphcmd(event):
         await xx.edit("Reply to a Message !")
 
 
-@ultroid_cmd(pattern="json")
+@Asteroid_cmd(pattern="json")
 async def _(event):
     the_real_message = None
     reply_to_id = None
@@ -501,7 +496,7 @@ async def _(event):
     if len(the_real_message) > 4096:
         with io.BytesIO(str.encode(the_real_message)) as out_file:
             out_file.name = "json-ult.txt"
-            await ultroid_bot.send_file(
+            await Asteroid_bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -513,12 +508,12 @@ async def _(event):
         await eor(event, f"```{the_real_message}```")
 
 
-@ultroid_cmd(pattern="suggest")
+@Asteroid_cmd(pattern="suggest")
 async def sugg(event):
     if await event.get_reply_message():
         msgid = (await event.get_reply_message()).id
         try:
-            await ultroid.send_message(
+            await Asteroid.send_message(
                 event.chat_id,
                 file=InputMediaPoll(
                     poll=Poll(
@@ -544,7 +539,7 @@ async def sugg(event):
         )
 
 
-@ultroid_cmd(pattern="ipinfo ?(.*)")
+@Asteroid_cmd(pattern="ipinfo ?(.*)")
 async def ipinfo(event):
     xx = await eor(event, get_string("com_1"))
     ip = event.text.split(" ")
